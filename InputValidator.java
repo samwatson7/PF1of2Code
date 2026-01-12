681696790a686cbf5112ccd46dc3acfd3161db28
samwatson7 <30259128@blackpool.ac.uk>
2026-01-12 19:14:30 +0000

    Completed foundational part of Input Validator class

--- InputValidator.java
+++ InputValidator.java
@@ -1,7 +1,9 @@
 public class InputValidator {
 
-//Method to Validate an Integer Input
-public static Integer isValidInteger(String intInput, int rangeMinimum, int rangeMaximum) {
+/* Method to validate an Integer Input. If int is a true integer and can be converted from string 
+to int, and the value is within its range, the int value will be returned. Else, 'null' will be returned*/
+
+public static Integer validateInteger(String intInput, int rangeMinimum, int rangeMaximum) {
     try {
         int convertedIntInput = Integer.parseInt(intInput);
 
@@ -14,5 +16,27 @@ public static Integer isValidInteger(String intInput, int rangeMinimum, int rang
     } catch (NumberFormatException e) {
         return null;
     }
+}
+
+/*Method to validate String Input. If the input isn't 'null', empty or above maximum character length, 
+the cleaned String will be returned for use. Else, 'null' will be returned.*/
+
+public static String validateString(String stringInput, int maximumLength) {
+
+    if (stringInput == null) {
+        return null;
+    }
+
+    String trimmedStringInput = stringInput.trim();
+
+    if (trimmedStringInput.isEmpty()) {
+        return null;
+    }
+
+    if (trimmedStringInput.length() > maximumLength) {
+        return null;
+    }
+
+    return trimmedStringInput;
 }
 }
