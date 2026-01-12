92b9d079d610209719e2640de189aae062682da9
samwatson7 <30259128@blackpool.ac.uk>
2026-01-12 19:41:43 +0000

    Created field instances and constructor method for InventoryLogger

--- InventoryLogger.java
+++ InventoryLogger.java
@@ -1,3 +1,26 @@
+//File Imports
+
+import java.io.File;            //Enables the representaion of the log file      
+import java.io.FileWriter;      //Enables text to be written to log file
+import java.io.IOException;     //Enables file errors to be handled safely
+import java.time.LocalDateTime; //Enables access to current date and time
+
 public class InventoryLogger {
+
+    /*Private field attribute (encapsulation), prevents other classes from directly manipulating file. 
+    'logFile' is the file where logs are written.*/
+
+    private File logFile;
+
+    /*Constructor method. Accepts file name, and creates a new File object. The logger now knows where 
+    to log.*/
+
+    public InventoryLogger(String fileName) {
+        logFile = new File(fileName);
+    }
+
+    public void logAction() {
+
+    }
     
 }
